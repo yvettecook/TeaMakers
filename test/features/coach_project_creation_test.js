c918d7522989b67c66cce4c0185231c50ae06aff
Andrew Hercules <andrewhercules@gmail.com>
2014-11-11 20:40:59 +0000

    Created basic views for page to create makeathon and manage makathon

--- test/features/coach_project_creation_test.js
+++ test/features/coach_project_creation_test.js
@@ -6,23 +6,31 @@ var http = require('http')
 var chai = require('chai');
 var mocha = require('mocha');
 var expect = chai.expect;
-
+var assert = require('assert')
 
 describe('coach makeathon creation form', function() {
 
   before(function() {
     this.server = http.createServer(app).listen(5000);
-    this.browser = new Browser({ site: 'http://localhost:5000' });
-    console.log('Hello')
+    browser = new Browser({ site: 'http://localhost:5000' });
   });
 
-  before(function(done) {
-    this.browser.visit('/', done);
+  beforeEach(function(done) {
+    browser.visit('/createmakeathon', done);
+  });
+
+  after(function(done){
+    this.server.close(done);
   });
 
   it('should have a header "Create makeathon"', function(){
-    this.browser.visit('/createmakeathon', done);
-    expect(this.browser.text('h1')).to.equal('Create makeathon')
+    expect(browser.text('h1')).to.equal('Create makeathon');
+  });
+
+  it('should have a field to enter the name of the makeathon', function() {
+      browser.pressButton('Submit');
+        //.fill('name', 'September 2014 Makeathon')
+    expect(browser.text('h1')).to.equal('Success!');
   });
 
 });
