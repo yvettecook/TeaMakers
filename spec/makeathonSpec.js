c6a73199f053a8f1f3731e59e26d298732166ee5
Victoria Barr <vb329@georgetown.edu>
2014-11-11 19:44:57 +0000

    Made a makeathon class; it has a name

---
+++ spec/makeathonSpec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var Makeathon = require('../src/makeathon');
+
+
+
+describe('a makeathon', function(){
+
+	it('should have a name', function(){
+		var september = new Makeathon('September 2014');
+		expect(september.name).toEqual('September 2014')
+	});
+});
---
+++ src/makeathon.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var makeathon = function Makeathon(name){
+	this.name = name;
+};
+
+
+module.exports = makeathon
+
+
