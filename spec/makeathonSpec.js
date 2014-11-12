c3b57d701bcebde98e468b380a7abb650778f32e
Victoria Barr <vb329@georgetown.edu>
2014-11-12 14:43:14 +0000

    Has student array that it can add students to

--- spec/makeathonSpec.js
+++ spec/makeathonSpec.js
@@ -5,6 +5,10 @@ var september = null
 
 describe('a makeathon', function(){
 
+	function isArray(object){
+		return Object.prototype.toString.call(object) == '[object Array]';
+	}
+
 	beforeEach(function(){
 		september = new Makeathon({ name: 'September 2014', projects: ['kickstarter', 'git racer'] });
 	});
@@ -23,15 +27,24 @@ describe('a makeathon', function(){
 	});
 
 	it('has an array of projects', function(){
-		expect(september.projects).toMatch([]);
+		expect(isArray(september.projects)).toBe(true)
 	});
 
 	it('can access individual projects', function(){
 		expect(september.projects[0]).toEqual('kickstarter')
 	});
 
-	it('can add projects to its array', function(){
+	it('can add projects to its project array', function(){
 		september.addProject('hairnado');
 		expect(september.projects[2]).toEqual('hairnado')
+	});
+
+	it('has an array of students', function() {
+		expect(isArray(september.students)).toBe(true)
+	});
+
+	it('can add a student to its student array', function(){
+		september.addStudent('Hercules')
+		expect(september.students[0]).toEqual('Hercules')
 	});
 });
