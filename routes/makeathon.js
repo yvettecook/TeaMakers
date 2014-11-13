70bf31d1e63f8990ebd900b956495801d41f5487
Andrew Hercules <andrewhercules@gmail.com>
2014-11-13 10:01:21 +0000

    Fixed route issue when coach form posted to makeathon/create page

--- routes/makeathon.js
+++ routes/makeathon.js
@@ -1,27 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-// router.get('/', function(req, res) {
-//   res.render('welcomepage');
-// });
-
-// router.post('/makeathonmanagement', function(req, res) {
-// });
-
 router.get('/create', function(req, res) {
   res.render('createmakeathon');
 });
 
-// router.post('/create', function(req, res) {
-//   makeathonProject.save({
-//       makeathon_name: req.param('makeathon_name')
-//       project_name: req.param('project_name')
-//       project_details: req.param('project_details')
-//   }, function(error, docs) {
-//       res.redirect('/')
-//     });
-// });
+router.post('/create', function(req, res) {
+  console.log(req.body); //provides all params info in an object
+  res.redirect('/makeathon/manage')
+});
+
+router.get('/manage', function(req, res) {
+  res.render('makeathonmanagement');
+  console.log(req.body);
+});
+
+  // makeathonProject.save({
+  //     makeathon_name: req.param('makeathon_name')
+  //     project_name: req.param('project_name')
+  //     project_details: req.param('project_details')
+  // }, function(error, docs) {
+  //     res.redirect('/')
+  //   });
 
 // router.get('/manage', function(req, res) {
 //   console.log(req.params);
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var expressLayouts = require('express-ejs-layouts');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -14,6 +15,7 @@ var server = require('http').Server(app);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
