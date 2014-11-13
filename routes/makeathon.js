8c29f99229db453beb9042999500068ef63ce115
Andrew Hercules <andrewhercules@gmail.com>
2014-11-13 11:38:53 +0000

    Management page shows name based on params - moving towards RESTFUl structure

--- routes/makeathon.js
+++ routes/makeathon.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var makeathons = [
+  { name : 'september',
+    description: 'flippin awesome cohort' }
+]
 
 router.get('/create', function(req, res) {
   res.render('createmakeathon');
@@ -8,14 +12,19 @@ router.get('/create', function(req, res) {
 router.post('/create', function(req, res) {
   console.log(req.body); //provides all params info in an object
   console.log(req.body.makeathon_name)
-  var makeathonName = req.body.makeathon_name
-  res.render('makeathonmanagement', {makeathonName: makeathonName} )
-  // res.redirect('/makeathon/manage')
+  var makeathon_name = req.body.makeathon_name
+  // res.render('makeathonmanagement', {makeathonName: makeathonName} )
+  res.redirect('/makeathon/:makeathon_name')
 });
 
 router.get('/manage', function(req, res) {
   res.render('makeathonmanagement');
   console.log(req.body);
+});
+
+router.get('/:name', function(req, res) {
+  var makeathon_name = req.params.name
+  res.render('makeathonmanagement', {makeathon_name : makeathon_name} );
 });
 
   // makeathonProject.save({
